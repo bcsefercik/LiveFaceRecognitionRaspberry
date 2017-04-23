2ab7dddb18219c6bcad5567288e4a2e2ae0869e8
Buğra Can Sefercik <bsefercik@gmail.com>
2017-04-24 00:08:11 +0300

    GUI start

--- App.py
+++ App.py
@@ -1,3 +1,17 @@
-from Recognizer import Recognizer
+from imutils.video import VideoStream
+from View import MainView
+import argparse
+import time
 
-recognizer = Recognizer("asdf")
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+	help="whether or not the Raspberry Pi camera should be used")
+opt = vars(ap.parse_args())
+
+print("[INFO] Launching camera")
+vs = VideoStream(usePiCamera=opt["picamera"] > 0).start()
+time.sleep(2)
+
+view = MainView(vs)
+
+view.root.mainloop()
---
+++ View.py
@@ -0,0 +1,43 @@
+from Recognizer import Recognizer
+import Tkinter as tki
+import threading
+import imutils
+
+class MainView:
+	def __init__(self, vs, width=320, height=480):
+		self.vs = vs
+
+		self.frame = None
+		self.thread = None
+		self.stopEvent = None
+
+		self.root = tki.Tk()
+		self.root.resizable(width=False, height=False)
+		self.root.geometry('{}x{}'.format(width, height))
+
+		self.button = tki.Button(self.root, text="Ring the Bell!", command=self.ring)
+		self.button.pack(side="bottom", fill="both", expand="yes", padx=10, pady=10)
+
+		self.stopEvent = threading.Event()
+		self.thread = threading.Thread(target=self.videoLoop, args=())
+		self.thread.start()
+
+		self.root.wm_title("Hoosthere")
+		self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
+
+	def videoLoop(self):
+		try:
+			a = 0
+			while True:
+				a += 1
+		except RuntimeError, e:
+			print("[INFO] Runtime Error")
+	
+	def ring(self):
+		return 0
+
+	def onClose(self):
+		print("[INFO] Closing")
+		self.stopEvent.set()
+		self.vs.stop()
+		self.root.quit()
