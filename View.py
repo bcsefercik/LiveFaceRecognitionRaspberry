4c14235974939629a653565e28f8cc0a7f38ad9e
Buğra Can Sefercik <bsefercik@gmail.com>
2017-04-25 01:17:55 +0300

    a

--- View.py
+++ View.py
@@ -11,7 +11,7 @@ class MainView:
 	def __init__(self, vs, width=320, height=450, framerate=32):
 		
 		self.vs = vs
-		self.outputPath = outputPath
+		self.outputPath = "outputPath"
 		self.frame = None
 		self.thread = None
 		self.stopEvent = None
@@ -30,41 +30,29 @@ class MainView:
 		btn.pack(side="bottom", fill="both", expand="yes", padx=10,
 			pady=10)
 
-		# start a thread that constantly pools the video sensor for
-		# the most recently read frame
 		self.stopEvent = threading.Event()
 		self.thread = threading.Thread(target=self.videoLoop, args=())
 		self.thread.start()
 
-		# set a callback to handle when the window is closed
 		self.root.wm_title("PyImageSearch PhotoBooth")
 		self.root.wm_protocol("WM_DELETE_WINDOW", self.onClose)
 
+		self.recognizer = Recognizer()
+
 	def videoLoop(self):
-		# DISCLAIMER:
-		# I'm not a GUI developer, nor do I even pretend to be. This
-		# try/except statement is a pretty ugly hack to get around
-		# a RunTime error that Tkinter throws due to threading
 		try:
-			# keep looping over frames until we are instructed to stop
 			while not self.stopEvent.is_set():
-				# grab the frame from the video stream and resize it to
-				# have a maximum width of 300 pixels
 				self.frame = self.vs.read()
-				self.frame = imutils.resize(self.frame, width=300)
+				self.frame = imutils.resize(self.frame, width=self.panelWidth)
 		
-				# OpenCV represents images in BGR order; however PIL
-				# represents images in RGB order, so we need to swap
-				# the channels, then convert to PIL and ImageTk format
 				image = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
 				image = Image.fromarray(image)
 				image = ImageTk.PhotoImage(image)
 		
-				# if the panel is not None, we need to initialize it
 				if self.panel is None:
 					self.panel = tki.Label(image=image)
 					self.panel.image = image
-					self.panel.pack(side="left", padx=10, pady=10)
+					self.panel.pack(side="left", padx=0, pady=10)
 		
 				# otherwise, simply update the panel
 				else:
